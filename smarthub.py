0192b3d8d8e321e36289d697bb9a56f6f314175d
Son Tieu <tieuthanhson.lpt@gmail.com>
2016-03-26 15:42:50 +0700

    test heroku

--- smarthub.py
+++ smarthub.py
@@ -17,7 +17,7 @@ TYPE_DEVICE = ["LED"]
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-
+'''
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -25,8 +25,7 @@ def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
-        db.commit()    
-
+        db.commit() 
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -78,6 +77,21 @@ def detail_device(device_id):
     cur = g.db.execute('select id, name from devices where id = (?)', [device_id])
     device = [dict(id=row[0], name=row[1]) for row in cur.fetchall()]
     return render_template('detail_device.html', device=device[0])
+
+@app.route('/save', methods=['POST'])
+def save_config():
+    data = request.form['feature']
+    flash('data = ' + data)
+    data = data.split('_')
+    flash('status = ' + data[0] + ' id = ' + data[1])
+    g.db.execute('insert into services (status, device_id) valueS (?,?)', [data[0], data[1]])
+    g.db.commit()
+    return redirect(url_for('show_devices'))
+'''
+@app.route('/')
+def show_devices():
+    return render_template('show_devices.html')
+
 
 if __name__ == '__main__':
     app.run()
